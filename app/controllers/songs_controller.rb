ed3e51fc6c4a6b83a533f4448ad905da127d846d
sukey <sdklos@gmail.com>
2017-08-13 14:54:20 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -25,12 +25,19 @@ class SongsController < ApplicationController
   end
 
   def new
-    @song = Song.new
+    if params[:artist_id] && !Artist.exists?(params[:artist_id])
+      redirect_to artists_path, alert: "Artist not found."
+    else
+      @song = Song.new(artist_id: params[:artist_id])
+    end
   end
 
   def create
-    @song = Song.new(song_params)
-
+    if params[:artist_id] && !Artist.exists?(params[:artist_id])
+      redirect_to artists_path, alert: "Artist not found."
+    else
+      @song = Song.new(song_params)
+    end
     if @song.save
       redirect_to @song
     else
@@ -39,14 +46,30 @@ class SongsController < ApplicationController
   end
 
   def edit
-    @song = Song.find(params[:id])
+    if params[:artist_id]
+      @artist = Artist.find_by(id: params[:artist_id])
+      if @artist.nil?
+        redirect_to artists_path, alert: "Artist not found."
+      else
+        @song = @artist.songs.find_by(id: params[:id])
+        redirect_to artist_songs_path(@artist), alert: "Song not found." if @song.nil?
+      end
+    else
+      @song = Song.find(params[:id])
+      @artists = Artist.all
+    end
   end
 
   def update
     @song = Song.find(params[:id])
-
-    @song.update(song_params)
-
+    @artist = Artist.find(params[:artist_id])
+    if params[:artist_id] && !@artist
+      redirect_to artists_path, alert: "Artist not found."
+    elsif params[:artist_id] && !@artist.songs.include?(@song)
+      redirect_to artists_songs_path(@artist)
+    else
+      @song.update(song_params)
+    end
     if @song.save
       redirect_to @song
     else
@@ -64,7 +87,6 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title, :artist_name)
+    params.require(:song).permit(:title, :artist_name, :artist_id)
   end
 end
-
--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,9 @@
 module SongsHelper
-
+  def artist_select(song, artist)
+    if song.artist.nil?
+      select_tag "song[artist_id]", options_from_collection_for_select(@artists, :id, :name)
+    else
+      hidden_field_tag "song[artist_id]", song.artist_id
+    end
+  end
 end
